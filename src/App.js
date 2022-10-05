db1a18b5e36a7605679ad28bd227630596df7575
sanduni <cst18045@std.uwu.ac.lk>
2022-10-05 22:10:19 +0530

    dish updated  add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import React, { Component } from "react";
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './Components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-      </Navbar>
-      <Menu/>
+        </Navbar>
+     
+        <Menu dishes={this.state.dishes} />
       </div>
-  );
+    );
+  }
 }
-
 export default App;
